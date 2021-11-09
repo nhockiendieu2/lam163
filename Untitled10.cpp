c1665eb45db470a4e64cbd603db5c87e0dda4740
nhockiendieu2 <93941676+nhockiendieu2@users.noreply.github.com>
2021-11-09 11:14:10 +0700

    Create Untitled10.cpp
    bai tap c

---
+++ Untitled10.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+  long S;
+  S = 0;
+  n = 1;
+  printf("\nNhap vào so n: ");
+  scanf("%d", &n);
+ 
+  for(int i = 1; i <=n; i+=2)
+    {
+        S = S + i;
+    }
+  printf("\nTong 1 + 3 +5 ... + (S+2*i+1)%d là %ld: ", n, S);
+ 
+  printf("\n----------------------------------------\n");
+}
+
